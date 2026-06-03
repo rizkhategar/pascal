f1e626d45d388fcf75c18d654fc32ebcf549d540
dimszky <schooldims@gmail.com>
2026-06-03 16:07:25 +0700

    favicon

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pascasarjana Universitas Ngudi Waluyo</title>
-
+    <link rel="icon" href="{{ asset('logo_unwnobg.png') }}" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
     <style>
