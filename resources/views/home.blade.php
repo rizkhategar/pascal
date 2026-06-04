77bc974459da8183b66d6714b2e545b7a4d986de
AmmaarSiraj <ammarsirajnomor1pl@gmail.com>
2026-06-04 10:16:03 +0700

    back to vanila

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Pascasarjana Universitas Ngudi Waluyo</title>
     <link rel="icon" href="{{ asset('logo_unwnobg.png') }}" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
