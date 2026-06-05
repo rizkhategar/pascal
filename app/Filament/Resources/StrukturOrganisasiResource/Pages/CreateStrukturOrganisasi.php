0a618caaabce367b85f17ec744de1d7a88ee00c6
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-05 09:16:19 +0700

    Tambah halaman create resource struktur organisasi

---
+++ app/Filament/Resources/StrukturOrganisasiResource/Pages/CreateStrukturOrganisasi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\StrukturOrganisasiResource\Pages;
+
+use App\Filament\Resources\StrukturOrganisasiResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateStrukturOrganisasi extends CreateRecord
+{
+    protected static string $resource = StrukturOrganisasiResource::class;
+}
