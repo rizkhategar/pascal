ac3270fe77ede42884f56e90204e1279119697bd
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-05 13:33:52 +0700

    Urutkan grup navigasi profil lalu akademik

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -32,6 +33,15 @@ class AdminPanelProvider extends PanelProvider
             ->brandName('Pascasarjana UNW')
             ->colors([
                 'primary' => Color::Amber,
+            ])
+            ->navigationGroups([
+                NavigationGroup::make()
+                    ->label('Profil')
+                    ->collapsed(false),
+
+                NavigationGroup::make()
+                    ->label('Akademik')
+                    ->collapsed(false),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
