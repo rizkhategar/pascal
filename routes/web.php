398683b9acee1aff3748fdbad66ae25e2723f361
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-05 11:24:06 +0700

    Tambahkan route halaman upload biasa struktur organisasi

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,15 @@ Route::get('/', function () {
 Route::get('/akademik/{slug}', [AcademicController::class, 'show'])->name('akademik.show');
 
 Route::get('/akademik/magister-hukum', function () {
-    // Pastikan file akademik.blade.php berada di dalam folder resources/views/
-    return view('akademik'); 
+    return view('akademik');
 })->name('akademik.hukum');
 
 Route::get('/visi-misi', [VisiMisiController::class, 'index'])->name('visi-misi');
 Route::get('/profil/struktur-organisasi', [StrukturOrganisasiController::class, 'index'])->name('profil.struktur-organisasi');
 
 Route::middleware(['web', 'auth'])->group(function () {
+    Route::get('/admin/struktur-organisasis/custom-create', [StrukturOrganisasiUploadController::class, 'create'])->name('admin.struktur-organisasi-upload.create');
+    Route::get('/admin/struktur-organisasis/{strukturOrganisasi}/custom-edit', [StrukturOrganisasiUploadController::class, 'edit'])->name('admin.struktur-organisasi-upload.edit');
     Route::post('/admin/struktur-organisasis/upload', [StrukturOrganisasiUploadController::class, 'store'])->name('admin.struktur-organisasi-upload.store');
     Route::put('/admin/struktur-organisasis/{strukturOrganisasi}/upload', [StrukturOrganisasiUploadController::class, 'update'])->name('admin.struktur-organisasi-upload.update');
 });
