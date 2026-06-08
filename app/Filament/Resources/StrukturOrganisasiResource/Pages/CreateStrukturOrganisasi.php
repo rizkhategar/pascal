ce23ba78a1a20929018261f024db21bfef088d94
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-08 10:36:25 +0700

    Gunakan page Filament asli untuk create Struktur Organisasi

--- app/Filament/Resources/StrukturOrganisasiResource/Pages/CreateStrukturOrganisasi.php
+++ app/Filament/Resources/StrukturOrganisasiResource/Pages/CreateStrukturOrganisasi.php
@@ -3,9 +3,16 @@
 namespace App\Filament\Resources\StrukturOrganisasiResource\Pages;
 
 use App\Filament\Resources\StrukturOrganisasiResource;
-use Filament\Resources\Pages\CreateRecord;
+use Filament\Resources\Pages\Page;
 
-class CreateStrukturOrganisasi extends CreateRecord
+class CreateStrukturOrganisasi extends Page
 {
     protected static string $resource = StrukturOrganisasiResource::class;
+
+    protected string $view = 'filament.resources.struktur-organisasi-resource.pages.create-struktur-organisasi';
+
+    public function getTitle(): string
+    {
+        return 'Tambah Struktur Organisasi';
+    }
 }
