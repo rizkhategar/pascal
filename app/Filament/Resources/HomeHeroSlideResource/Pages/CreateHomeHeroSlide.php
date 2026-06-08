0652976f4e50948c4b96ffe92ecfc539bc012663
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-08 09:10:12 +0700

    Tambah halaman create resource Hero Campus

---
+++ app/Filament/Resources/HomeHeroSlideResource/Pages/CreateHomeHeroSlide.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\HomeHeroSlideResource\Pages;
+
+use App\Filament\Resources\HomeHeroSlideResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateHomeHeroSlide extends CreateRecord
+{
+    protected static string $resource = HomeHeroSlideResource::class;
+}
