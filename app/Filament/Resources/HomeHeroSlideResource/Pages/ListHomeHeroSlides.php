463fba3462e02da3809e5091757ba0b47a8b84e0
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-08 09:09:45 +0700

    Tambah halaman list Hero Campus

---
+++ app/Filament/Resources/HomeHeroSlideResource/Pages/ListHomeHeroSlides.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filament\Resources\HomeHeroSlideResource\Pages;
+
+use App\Filament\Resources\HomeHeroSlideResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+
+class ListHomeHeroSlides extends ListRecords
+{
+    protected static string $resource = HomeHeroSlideResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make()
+                ->label('Tambah Hero Campus')
+                ->url(route('admin.home-hero-slides.create-custom')),
+        ];
+    }
+}
