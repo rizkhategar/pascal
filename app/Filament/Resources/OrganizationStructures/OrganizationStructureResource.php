b4c944b68dfbfbc0efec5c70a91b67001ebe7a14
Rizkha Tegar <rizkhategar@gmail.com>
2026-06-11 09:34:49 +0700

    Add organization structure resource in English

---
+++ app/Filament/Resources/OrganizationStructures/OrganizationStructureResource.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\OrganizationStructures;
+
+use App\Filament\Resources\OrganizationStructures\Pages\CreateOrganizationStructure;
+use App\Filament\Resources\OrganizationStructures\Pages\EditOrganizationStructure;
+use App\Filament\Resources\OrganizationStructures\Pages\ListOrganizationStructures;
+use App\Filament\Resources\OrganizationStructures\Schemas\OrganizationStructureForm;
+use App\Filament\Resources\OrganizationStructures\Tables\OrganizationStructuresTable;
+use App\Models\OrganizationStructure;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Tables\Table;
+use UnitEnum;
+
+class OrganizationStructureResource extends Resource
+{
+    protected static ?string $model = OrganizationStructure::class;
+
+    protected static ?string $slug = 'organization-structures';
+
+    protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-building-office-2';
+
+    protected static ?string $navigationLabel = 'Organization Structure';
+
+    protected static ?string $modelLabel = 'Organization Structure';
+
+    protected static ?string $pluralModelLabel = 'Organization Structures';
+
+    protected static string|UnitEnum|null $navigationGroup = 'Profile';
+
+    protected static ?int $navigationSort = 3;
+
+    public static function form(Schema $schema): Schema
+    {
+        return OrganizationStructureForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return OrganizationStructuresTable::configure($table);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListOrganizationStructures::route('/'),
+            'create' => CreateOrganizationStructure::route('/create'),
+            'edit' => EditOrganizationStructure::route('/{record}/edit'),
+        ];
+    }
+}
