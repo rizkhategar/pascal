99632a435a8eb097829445bb5fd703be55e8f9d6
dimszky <schooldims@gmail.com>
2026-06-12 11:16:07 +0700

    visimisi false

--- app/Filament/Resources/VisiMisis/Pages/CreateVisiMisi.php
+++ app/Filament/Resources/VisiMisis/Pages/CreateVisiMisi.php
@@ -3,9 +3,12 @@
 namespace App\Filament\Resources\VisiMisis\Pages;
 
 use App\Filament\Resources\VisiMisis\VisiMisiResource;
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateVisiMisi extends CreateRecord
 {
     protected static string $resource = VisiMisiResource::class;
+
+    protected static bool $canCreateAnother = false;
 }
